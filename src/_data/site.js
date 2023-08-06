e952f99c8a006d23494441c7086066266acd9994
Bob Monsour <bob.monsour@gmail.com>
2023-08-06 16:42:23 -0700

    Cleaner URLs, updated redirects

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,16 +4,16 @@ module.exports = {
   url: "https://www.faustotasca.com",
   mainNavLinks: [
     { url: "/", text: "Home", class: "nav-item1" },
-    { url: "/early-years.html", text: "Early Years", class: "nav-item2" },
-    { url: "/early-portfolio.html", text: "Early Portfolio", class: "nav-item3" },
-    { url: "/santa-barbara.html", text: "Santa Barbara", class: "nav-item4" },
-    { url: "/montecito.html", text: "Montecito", class: "nav-item5" },
-    { url: "/san-diego.html", text: "San Diego", class: "nav-item6" },
-    { url: "/pasadena.html", text: "Pasadena", class: "nav-item7" },
-    { url: "/los-angeles.html", text: "Los Angeles", class: "nav-item8" },
-    { url: "/portraits.html", text: "Portraits", class: "nav-item9" },
-    { url: "/sculpture.html", text: "Sculpture", class: "nav-item10" },
-    { url: "/plein-air.html", text: "Plein Air", class: "nav-item11" },
-    { url: "/contact.html", text: "Contact", class: "nav-item12" }
-  ]
+    { url: "/early-years/", text: "Early Years", class: "nav-item2" },
+    { url: "/early-portfolio/", text: "Early Portfolio", class: "nav-item3" },
+    { url: "/santa-barbara/", text: "Santa Barbara", class: "nav-item4" },
+    { url: "/montecito/", text: "Montecito", class: "nav-item5" },
+    { url: "/san-diego/", text: "San Diego", class: "nav-item6" },
+    { url: "/pasadena/", text: "Pasadena", class: "nav-item7" },
+    { url: "/los-angeles/", text: "Los Angeles", class: "nav-item8" },
+    { url: "/portraits/", text: "Portraits", class: "nav-item9" },
+    { url: "/sculpture/", text: "Sculpture", class: "nav-item10" },
+    { url: "/plein-air/", text: "Plein Air", class: "nav-item11" },
+    { url: "/contact/", text: "Contact", class: "nav-item12" },
+  ],
 };
